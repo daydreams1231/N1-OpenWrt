b57ee33afd17be24419f365fb67dfe7691dd67a3
daydreams <68049920+daydreams1231@users.noreply.github.com>
2025-03-17 20:24:20 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -5,11 +5,11 @@
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
-cp -fr clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -fr clone/amlogic/luci-app-amlogic feeds/luci/applications/
 
 # Clean packages
 rm -rf clone
